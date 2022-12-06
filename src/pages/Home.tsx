f3b023306de09507b947e02a9224fac7ee8b63d1
mustafa-bilen <mustafabilen6669@gmail.com>
2022-12-06 15:22:36 +0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,29 @@ import React, { useEffect, useState } from "react";
 import InputForm from "../components/InputForm";
 import TodoList from "../components/TodoList";
 
+const url = "https://638f2c869cbdb0dbe31f2356.mockapi.io/todos";
+
 const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const getTodos = async () => {
+    try {
+      const { data } = await axios.get<TodoType[]>(url);
+      console.log(data);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <div className="main">
       <InputForm />
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   );
 };
